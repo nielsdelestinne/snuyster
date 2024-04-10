fe2854220d0787d6a24a18b598e017441aa225b4
nielsdelestinne <nielsdelestinne@gmail.com>
2024-04-10 22:14:48 +0200

    refactored selection menu

--- src/components/menu.sh
+++ src/components/menu.sh
@@ -9,46 +9,42 @@ echo "║ ║ ░░░░░░░░░░░░░░░░░░░░░░
 echo "║ ╚══════════════════════════════════════════════════════════════════════════════════════╝ ║"
 echo "╚════╗ ╔═══════════════════════════════════════════════════════════════════════════════════╝"
 
-COLUMNS=12
-options_echo_prefix="  ║ ║ "
-options_echo_prefix_last="  ╚═╝ "
-listRepositoriesOption="${options_echo_prefix}➊ List repositories"
-performSummaryAnalysisOption="${options_echo_prefix}➋ Perform summary analysis"
-performFullAnalysisOption="${options_echo_prefix}➌ Perform extended analysis"
-performSingleAnalysisOption="${options_echo_prefix}➍ Perform single analysis"
-quitOption="${options_echo_prefix_last}✗ Quit"
-options=("${listRepositoriesOption}" "${performSummaryAnalysisOption}" "${performFullAnalysisOption}" "${performSingleAnalysisOption}" "${quitOption}")
+echo "${echo_prefix}"
+echo -e "${echo_prefix}1) List repositories"
+echo -e "${echo_prefix}2) Perform summary analysis"
+echo -e "${echo_prefix}3) Perform extended analysis"
+echo -e "${echo_prefix}4) Perform single analysis"
+echo -e "${echo_prefix}5) Quit"
 
 echo "${echo_prefix}"
-echo "${echo_prefix}Make your selection:"
-echo "${echo_prefix}"
+read -rp "     ╚═╝ Make your selection (number): " option
 
-select opt in "${options[@]}"
-do
-    case $opt in
-        "${listRepositoriesOption}")
-            source "${workspace}/src/components/list_repositories.sh"
-            source "${workspace}/src/components/menu.sh"
-            break
-            ;;
-        "${performSummaryAnalysisOption}")
-            source "${workspace}/src/components/perform_summary_analysis.sh"
-            source "${workspace}/src/components/menu.sh"
-            break
-            ;;
-        "${performFullAnalysisOption}")
-            source "${workspace}/src/components/perform_extended_analysis.sh"
-            source "${workspace}/src/components/menu.sh"
-            break
-            ;;
-        "${performSingleAnalysisOption}")
-            source "${workspace}/src/components/perform_single_analysis.sh"
-            source "${workspace}/src/components/menu.sh"
-            break
-            ;;
-        "${quitOption}")
-            exit
-            ;;
-        *) echo "invalid option $REPLY. Please provide a valid option number.";;
-    esac
-done
+case $option in
+  1)
+      source "${workspace}/src/components/list_repositories.sh"
+      source "${workspace}/src/components/menu.sh"
+      break
+      ;;
+  2)
+      source "${workspace}/src/components/perform_summary_analysis.sh"
+      source "${workspace}/src/components/menu.sh"
+      break
+      ;;
+  3)
+      source "${workspace}/src/components/perform_extended_analysis.sh"
+      source "${workspace}/src/components/menu.sh"
+      break
+      ;;
+  4)
+      source "${workspace}/src/components/perform_single_analysis.sh"
+      source "${workspace}/src/components/menu.sh"
+      break
+      ;;
+  exit)
+      exit
+      ;;
+  \?) # Invalid option
+      echo "Error: Invalid option provided. Please provide a number or exit"
+      break
+      ;;
+esac
